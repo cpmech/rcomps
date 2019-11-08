f8357956a06d2b4a26343438627840cf9bb4a4fd
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-09 08:58:23 +1000

    Update docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5ce7c0bdb8c6c218d9869c1588fc5e76.js"
+  "precache-manifest.78ce61b1e6894d5138a4d2ea453d2a8e.js"
 );
 
 self.addEventListener('message', (event) => {
