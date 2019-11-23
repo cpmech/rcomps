4106a2bb6070e141463c2fd21f52775dd3c2dca3
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-23 13:13:36 +1000

    Update docs

---
+++ docs/precache-manifest.77a123315e344a746f63a34c8bd805bc.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e88a7979552a3afc7d83902833cdda",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0890afde76d40762a9a3.bundle.js"
+  },
+  {
+    "revision": "b0a1ed0a076cf96630eb",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.0890afde76d40762a9a3.bundle.js"
+  },
+  {
+    "revision": "31390f86c2bced2473228de7e186edce",
+    "url": "static/media/house.31390f86.svg"
+  },
+  {
+    "url": "vendors~main.0890afde76d40762a9a3.bundle.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a859631b764890b90476f4b83a93b5f5.js"
+  "precache-manifest.77a123315e344a746f63a34c8bd805bc.js"
 );
 
 self.addEventListener('message', (event) => {
