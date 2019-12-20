dd29c89368a8db511eaec7d93d7b625afbff8fba
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-12-20 19:11:09 +1000

    Update docs

--- docs/precache-manifest.83c13f55763860d91d1c52d4269e5eb6.js
+++ docs/precache-manifest.83c13f55763860d91d1c52d4269e5eb6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64bc77e1f779bce78aac1ee5f0ae3274",
+    "revision": "a78d7af63914c2b9024e94d509c3804c",
     "url": "iframe.html"
   },
   {
-    "url": "main.48bf79bd8eaa2384a10c.bundle.js"
+    "url": "main.80984dff33419d67179e.bundle.js"
   },
   {
-    "revision": "1326c54d880858e281e6",
+    "revision": "fc0766c383f46e0a8c90",
     "url": "main.css"
   },
   {
-    "url": "runtime~main.48bf79bd8eaa2384a10c.bundle.js"
+    "url": "runtime~main.80984dff33419d67179e.bundle.js"
   },
   {
     "revision": "31390f86c2bced2473228de7e186edce",
     "url": "static/media/house.31390f86.svg"
   },
   {
-    "url": "vendors~main.48bf79bd8eaa2384a10c.bundle.js"
+    "url": "vendors~main.80984dff33419d67179e.bundle.js"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a02e06950bf4a96e43a93af882eca53e.js"
+  "precache-manifest.83c13f55763860d91d1c52d4269e5eb6.js"
 );
 
 self.addEventListener('message', (event) => {
