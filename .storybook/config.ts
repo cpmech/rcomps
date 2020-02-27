4258cdc63dbe859913d106ce314a95a2e97afdb4
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-02-27 18:48:17 +1000

    Remove scss

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,4 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
-import '../src/styles/index.scss';
 
 configure(requireContext('../src', true, /\.stories\.tsx?$/), module);
