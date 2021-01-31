9ad9a0420a5fec1c3fcfa216ec218ccd68035678
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-31 14:40:05 +1000

    [wip] Update to React 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Router } from '@reach/router';
-import { Dashboard, Home, NotFound } from './pages';
+import logo from './logo.svg';
+import './App.css';
 
-export const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <Home path="/" />
-      <Dashboard path="/dashboard" />
-      <NotFound default />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
+
+export default App;
