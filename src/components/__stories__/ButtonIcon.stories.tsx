35e7f16d06ef2ec19c9bbe27d3b8c3b9ad1186f1
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-31 16:32:49 +1000

    [wip] Fix button icon stories

--- src/components/__stories__/ButtonIcon.stories.tsx
+++ src/components/__stories__/ButtonIcon.stories.tsx
@@ -1,42 +1,66 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { ButtonIcon, IButtonIconProps } from '../ButtonIcon';
 import {
+  IconAngleLeft,
+  IconAngleRight,
+  IconArrowLeft,
   IconGlobe,
   IconHouseThreeD,
-  IconArrowLeft,
-  IconAngleRight,
-  IconAngleLeft,
 } from '@cpmech/react-icons';
-import { ButtonIcon } from '../ButtonIcon';
 
-const stories = storiesOf('ButtonIcon', module);
+export default {
+  title: 'Components/ButtonIcon',
+  component: ButtonIcon,
+  argTypes: {
+    type: {
+      control: {
+        type: 'inline-radio',
+        options: ['button', 'submit', 'reset'],
+      },
+    },
+    onClick: { action: 'button-icon-clicked' },
+    backgroundColor: { control: 'color' },
+    color: { control: 'color' },
+    colorDisabled: { control: 'color' },
+    hoverColor: { control: 'color' },
+    hoverColorOutline: { control: 'color' },
+    height: { control: 'range', min: 0, max: 100, step: 1 },
+    paddingLeft: { control: 'range', min: 0, max: 100, step: 1 },
+    paddingRight: { control: 'range', min: 0, max: 100, step: 1 },
+    borderRadius: { control: 'range', min: 0, max: 100, step: 1 },
+    fontSize: { control: 'range', min: 0, max: 100, step: 1 },
+    disabled: { control: 'boolean' },
+    outline: { control: 'boolean' },
+    flatLeft: { control: 'boolean' },
+    flatRight: { control: 'boolean' },
+  },
+} as Meta;
 
-stories.addDecorator(withKnobs);
+const Template: Story<IButtonIconProps> = (args) => (
+  <ButtonIcon icon={<div>ICON GOES HERE</div>} {...args} />
+);
 
-stories.add('default', () => (
-  <ButtonIcon
-    onClick={action('Icon button clicked')}
-    icon={
-      <div>
-        <IconAngleLeft />
-        <IconHouseThreeD /> <IconArrowLeft /> <IconGlobe />
-        <IconAngleRight />
-      </div>
-    }
-    width="200px"
-  />
-));
+export const Default = Template.bind({});
 
-stories.add('go next', () => (
-  <ButtonIcon
-    onClick={action('Icon button clicked')}
-    icon={<IconAngleRight size={80} />}
-    width="100px"
-    height={100}
-    paddingLeft={4}
-    paddingRight={0}
-    borderRadius={300}
-  />
-));
+export const MoreIcons = Template.bind({});
+MoreIcons.args = {
+  ...Template.args,
+  icon: (
+    <div>
+      <IconAngleLeft />
+      <IconHouseThreeD /> <IconArrowLeft /> <IconGlobe />
+      <IconAngleRight />
+    </div>
+  ),
+};
+
+export const Arrow = Template.bind({});
+Arrow.args = {
+  ...Template.args,
+  icon: <IconAngleRight size={80} />,
+  width: '100px',
+  height: 100,
+  paddingLeft: 4,
+  paddingRight: 0,
+  borderRadius: 300,
+};
